8f5deb316c8c3529f778149ad1186d0a740c24d9
Dhanushka Mapagedara <mapagedaradlb.20@uom.lk>
2025-06-14 17:21:58 +0530

    css update

--- frontend/src/admin/admincomponents/sidebar.jsx
+++ frontend/src/admin/admincomponents/sidebar.jsx
@@ -4,17 +4,50 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="w-64 bg-blue-900 text-white min-h-screen p-4">
-      <h2 className="text-xl font-bold mb-6">Admin Panel</h2>
-      <ul className="space-y-4">
-        <li><NavLink to="/admin/dashboard">Dashboard</NavLink></li>
-        <li><NavLink to="/admin/birth-requests">Birth Requests</NavLink></li>
-        <li><NavLink to="/admin/marriage-requests">Marriage Requests</NavLink></li>
-        <li><NavLink to="/admin/death-requests">Death Requests</NavLink></li>
-        <li><NavLink to="/admin/upload">Uploads</NavLink></li>
-        <li><NavLink to="/admin/settings">Settings</NavLink></li>
-        <li><NavLink to="/login">Logout</NavLink></li>
+   <div className="w-64 bg-blue-900 text-white min-h-screen flex flex-col justify-between p-4">
+      {/* Menu Links */}
+      <ul className="space-y-5 text-2xl">
+        <li>
+          <NavLink to="/admin/dashboard" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin/birth-requests" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Birth Requests
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin/marriage-requests" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Marriage Requests
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin/death-requests" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Death Requests
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin/upload" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Uploads
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin/settings" className="block py-2 rounded hover:bg-amber-300 transition duration-300">
+            Settings
+          </NavLink>
+        </li>
       </ul>
+
+      {/* Logout */}
+      <div className="">
+        <NavLink
+          to="/login"
+          className="block bg-red-700 text-white text-center py-2 px-4 rounded hover:bg-red-500 transition duration-300"
+        >
+          Logout
+        </NavLink>
+      </div>
     </div>
   );
 };
