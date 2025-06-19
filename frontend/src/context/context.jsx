1419653431a32c228e92b1a68f9f3f36587ca71b
Dhanushka Mapagedara <mapagedaradlb.20@uom.lk>
2025-06-19 21:26:57 +0530

    local storage  data store

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,5 +1,5 @@
 
-import React, { createContext, useContext,useState} from "react";
+import React, { createContext, useContext,useState,useEffect} from "react";
 import  initialUserData from "../constants/user";
 
 
@@ -8,7 +8,17 @@ const UserContext = createContext();
 
 const context = ({children}) => {
 
-    const [userData, setUserData] = useState(initialUserData);
+
+
+    const [userData, setUserData] = useState(() => {
+    const stored = localStorage.getItem("userData");
+    return stored ? JSON.parse(stored) : initialUserData;
+  });
+
+ useEffect(() => {
+  localStorage.setItem('userData', JSON.stringify(userData));
+},[userData]);
+
   return (
 
     <UserContext.Provider value={{userData, setUserData}}>
